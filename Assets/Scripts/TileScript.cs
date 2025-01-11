fc0d7736fc7e9528aceb07a26da03aad30e90871
tearsofdespair <mealiv2004@gmail.com>
2025-01-11 18:19:00 +0300

    made Tile States

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/TileScript", fileName = "New Tile Script")]
+public class TileScript : ScriptableObject
+{
+    public Color BackgroundColor;
+    public Color TextColor;
+}
